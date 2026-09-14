d697dbcd5ea45fecdceb2b39667eb5ed70f89d28
Urbish <Urbish Mulmi". itsformaills@gmail.com>
2026-09-14 07:27:19 +0545

    Match frontend baseURL to  hosted backend

--- frontend/vite-project/src/api/apiInstance.js
+++ frontend/vite-project/src/api/apiInstance.js
@@ -4,7 +4,10 @@
 
 import axios from 'axios'  ;
 const port = 4000
-const baseURL = `http://localhost:${port}/api`
+// for local development, use the following baseURL
+// const baseURL = `http://localhost:${port}/api`
+// for online deployment, use the following baseURL
+const baseURL = "https://training-management-system-1uwq.onrender.com/api";
 
 const api = axios.create({
   baseURL: baseURL,
